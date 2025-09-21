6c0e6cf773d375d6c53bd53c6549e78331e43612
bakri-albahri <bakri.albahri.info@gmail.com>
2025-09-21 20:04:47 +0300

    upload files

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+
+function App() {
+  const [advice, setAdvice] = useState('')
+
+  useEffect(() => {
+    axios.get('https://api.adviceslip.com/advice')
+    .then((data) => setAdvice(data.data.slip.advice))
+    .catch((error) => console.log(error))
+    .finally(console.log("first"))
+  }, [advice])
+  return (
+    <div className='app'>
+      <div className="card">
+          <h1 className='advice'>{advice && advice}</h1>
+          <button onClick={() => setAdvice("")}>
+            <span>GIVE ME ADVICE!</span>
+          </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
